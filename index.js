43912643020c9d91fc6df1adc9d35ddb13ddb83d
ricky3knowhere <ricky03senju@gmail.com>
2020-09-07 09:32:42 +0700

    finish

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 
 const config = require('./config')
 const login = require('./routes/login')
+const checkLoggedIn = require('./helpers/checkLoggedIn')
 
 const hbs = require('express-hbs');
 const bodyParser = require('body-parser')
@@ -13,42 +14,56 @@ const SessionStore = sessionStore(session.Store);
 
 const db = require('./models')
 
+const users = require('./routes/users')
+
 const sequelizeSessionStore = new SessionStore({
   db: db.sequelize
 });
 
+
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  store: sequelizeSessionStore,
+  secret: 'anystringvalue',
   saveUninitialized: true,
-  cookie: { secure: true }
+  cookie: { secure: 'auto' },
+  resave: false,
+  store: sequelizeSessionStore
 }))
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 app.engine('hbs', hbs.express4({
-  partialsDir: __dirname + '/views/pages',
-  defaultLayout: __dirname + '/views/layouts/default'
+  partialsDir: __dirname + '/views/components',
+  defaultLayout: __dirname + '/views/layouts/default',
+  layoutsDir: __dirname + '/views/layouts'
 }));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
+
 app.get('/login', login.get)
 app.post('/login', login.post)
+
 app.get('/logout', (req, res) => {
   req.session.destroy(() => {
     res.redirect('login')
   })
 })
 
-app.get('/dashboard', (req, res) => {
-  res.render('dashboard')
-})
-
 app.get('/', (req, res) => {
   res.redirect('login')
 })
+
+app.get('/dashboard', checkLoggedIn, (req, res) => {
+  res.render('pages/dashboard')
+})
+
+app.get('/users', users.list)
+app.get('/users/new', users.new_get)
+app.post('/users/new', users.new_post)
+app.get('/users/:id', users.details)
 
 app.listen(config.port, () => {
   console.log('Server running at port '+ config.port);
--- routes/login.js
+++ routes/login.js
@@ -2,16 +2,17 @@ const db = require('../models')
 const bcrypt = require('bcrypt')
 
 const get = (req, res) => {
-  res.render('login')
+  res.render('pages/login')
 }
 
 const post = async (req, res) => {
   const user = await db.User.findOne({ where: { email: req.body.email }})
 
   if(user && bcrypt.compareSync(req.body.password, user.dataValues.password)){
-    req.session.name = user
-    req.session.save(() => res.redirect('/dashboard'))
-    
+    req.session.user = 'user'
+    req.session.save(() => res.redirect('/'))
+    // res.send(user)
+
   }else {
     res.redirect('/login')
   }
